9e6e4063a76073f7caa51fc7ff3f21699b3f138f
Andrey Celestino <lab53@fatec.com>
2025-03-17 09:25:36 -0300

    1.0

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Va pressionando qualquer tecla para contar!");
+Console.ReadKey();
+Console.WriteLine("Preparar...");
+Console.ReadKey();
+Console.WriteLine("Apontar...");
+Console.ReadKey();
+Console.WriteLine("FOGO!");
